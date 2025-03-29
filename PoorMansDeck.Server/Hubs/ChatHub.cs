2a37a7af29470cb0cdf0e3870c393018c1753bd2
usausa <machi.pon@gmail.com>
2025-03-29 17:33:20 +0900

    Update

--- PoorMansDeck.Server/Hubs/ChatHub.cs
+++ PoorMansDeck.Server/Hubs/ChatHub.cs
@@ -6,8 +6,8 @@ using PoorMansDeck.Event;
 
 public class ChatHub : Hub
 {
-    public async Task AddMessage(string text)
+    public Task AddMessage(string text)
     {
-        await Clients.All.SendAsync("ReceiveMessage", new ChatMessage { Text = text, Timestamp = DateTime.Now }).ConfigureAwait(false);
+        return Clients.All.SendAsync("ReceiveMessage", new ChatMessage { Text = text, Timestamp = DateTime.Now });
     }
 }
